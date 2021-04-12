5b85f0a617a06b10a89b051b463e1d870f7e8917
Kathryn <kshafer7724@sdsu.edu>
2021-04-11 17:20:37 -0700

    setting up the main file

---
+++ mizzo/Source.cpp
@@ -0,0 +1,15 @@
+/*
+    Daniel A. Silva Red ID: 820567716
+    Kathryn Shafer  Red ID: 821586552
+*/
+
+#include <iostream>
+#include <pthread.h>
+#include <stdio.h>
+#include <string>
+#include <semaphore.h>
+
+int main(int argc, char** argv)
+{
+
+}
