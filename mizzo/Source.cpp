fb67f3a4fe047655499e3a5b5cc33f28f3ac44b7
Daniel Albertazzi Silva <79875935+DasandySlipa@users.noreply.github.com>
2021-04-11 18:10:00 -0700

    Command line arguments now readable

--- mizzo/Source.cpp
+++ mizzo/Source.cpp
@@ -9,8 +9,31 @@
 #include <string>
 #include <semaphore.h>
 #include <unistd.h>
+using namespace std;
 
 int main(int argc, char** argv)
 {
+    int argVal = 0, eSlep = 0, lSlep = 0, cfbSlep = 0, eesSlep = 0;
 
+    while (argVal = getopt(argc, argc, "ELFe:") != -1) 
+    {
+        switch (argVal) 
+        {
+        case 'E':
+            eSlep = atoi(optarg);
+            break;
+        case 'L':
+            lSlep = atoi(optarg);
+            break;
+        case 'F':
+            cfbSlep = atoi(optarg);
+            break;
+        case 'e':
+            eesSlep = atoi(optarg);
+            break;
+        default:
+            cout << "Invalid optional argument" << endl;
+            exit(EXIT_FAILURE);
+        }
+    }
 }
